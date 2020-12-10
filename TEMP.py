bfa80e5c7927d51cb5ffc714c97e28178f37655e
piparsforever <72268957+piparsforever@users.noreply.github.com>
2020-12-10 19:43:37 +0200

    .

--- TEMP.py
+++ TEMP.py
@@ -1,32 +1,36 @@
-def over_nine_thousand(lst):
-    x = 0
-    for i in lst:
-        if x == 9000:
-            x += i
-        elif x < 9000:
-            x += i
-    return x
-
-
-print(over_nine_thousand([8000, 900, 120, 5000, 6000]))
-
-
-def over_nine_thousand(lst):
-    x = []
-    for i in lst:
-        x.append(i)
-        if sum(x) >= 9000:
-            return sum(x)
-        elif sum(lst) < 9000:
-            return sum(lst)
-        elif len(lst) == 0:
-            return 0
-
-
-print(over_nine_thousand([8000, 900, 120, 5000, 6000]))
+# def over_nine_thousand(lst):
+    # x = 0
+    # for i in lst:
+        # if x == 9000:
+            # x += i
+        # elif x < 9000:
+            # x += i
+    # return x
+#
+#
+# print(over_nine_thousand([8000, 900, 120, 5000, 6000]))
+#
+#
+# def over_nine_thousand(lst):
+    # x = []
+    # for i in lst:
+        # x.append(i)
+        # if sum(x) >= 9000:
+            # return sum(x)
+        # elif sum(lst) < 9000:
+            # return sum(lst)
+        # elif len(lst) == 0:
+            # return 0
+#
+#
+# print(over_nine_thousand([8000, 900, 120, 5000, 6000]))
 
 
 x = open("Txt_temp.rtf", "r")
-y = x.read()
-print("Charater count =", len(y))
-print(y[:20])
+for i in x:
+    i = i.rstrip("\n\n\n\n")
+    i = i.lower()
+    x = i.count("@")
+    if not "@" or i.count("@") < 5:
+        continue
+    print(x, i)
